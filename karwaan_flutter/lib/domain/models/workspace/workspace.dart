c869909d3e21e7a0a705480bde11cba75f6ea3ee
Misbah Golami <misbahgholami63@gmail.com>
2025-08-04 20:12:23 +0400

    Added created at field.

--- karwaan_flutter/lib/domain/models/workspace/workspace.dart
+++ karwaan_flutter/lib/domain/models/workspace/workspace.dart
@@ -2,9 +2,10 @@ class Workspace {
   final int id;
   final String workspaceName;
   final String workspaceDescription;
+  final DateTime createdAt;
 
   Workspace(
       {required this.id,
       required this.workspaceName,
-      required this.workspaceDescription});
+      required this.workspaceDescription, required this.createdAt});
 }
