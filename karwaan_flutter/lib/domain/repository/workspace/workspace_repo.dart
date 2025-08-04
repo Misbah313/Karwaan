c360fca0be669371274bd4a2db37820e6b9392c5
Misbah Golami <misbahgholami63@gmail.com>
2025-08-04 20:13:20 +0400

    Added get workspace members method.

--- karwaan_flutter/lib/domain/repository/workspace/workspace_repo.dart
+++ karwaan_flutter/lib/domain/repository/workspace/workspace_repo.dart
@@ -2,6 +2,7 @@ import 'package:karwaan_flutter/domain/models/workspace/create_workspace_credent
 import 'package:karwaan_flutter/domain/models/workspace/workspace.dart';
 import 'package:karwaan_flutter/domain/models/workspace/workspace_credentials.dart';
 import 'package:karwaan_flutter/domain/models/workspace/workspace_member_credentials.dart';
+import 'package:karwaan_flutter/domain/models/workspace/workspace_member_details.dart';
 import 'package:karwaan_flutter/domain/models/workspace/workspace_member_model.dart';
 
 abstract class WorkspaceRepo {
@@ -12,5 +13,6 @@ abstract class WorkspaceRepo {
    Future<void> deleteWorkspace(int workspaceId);
    Future<WorkspaceMemberModel> addMemberToWorkspace(WorkspaceMemberCredential workspaceMemberCredential);
    Future<void> removeMemberFromWorkspace(WorkspaceMemberCredential workspaceMemberCredential);
+   Future<List<WorkspaceMemberDetail>> getWorkspaceMembers(int workspaceId);
    Future<void> leaveWorkspace(int workspaceId);
 }
