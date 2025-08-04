79a56cac386fd39895ba778438a905e0e25408b6
Misbah Golami <misbahgholami63@gmail.com>
2025-08-04 20:10:57 +0400

    Modified

--- karwaan_flutter/lib/domain/models/workspace/create_workspace_credentials.dart
+++ karwaan_flutter/lib/domain/models/workspace/create_workspace_credentials.dart
@@ -1,7 +1,8 @@
 class CreateWorkspaceCredentials {
   final String workspaceName;
   final String workspaceDescription;
+  final DateTime createdAt;
 
   CreateWorkspaceCredentials(
-      {required this.workspaceName, required this.workspaceDescription});
+      {required this.workspaceName, required this.workspaceDescription, required this.createdAt});
 }
