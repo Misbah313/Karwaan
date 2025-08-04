a30d08aed78b9c4593404c1bafbfccae69e093f1
Misbah Golami <misbahgholami63@gmail.com>
2025-08-04 20:11:43 +0400

    Added user name and user role fields.

--- karwaan_flutter/lib/domain/models/workspace/workspace_member_credentials.dart
+++ karwaan_flutter/lib/domain/models/workspace/workspace_member_credentials.dart
@@ -1,6 +1,12 @@
 class WorkspaceMemberCredential {
   final int userId;
   final int workspaceId;
+  final String userName;
+  final String userRole;
 
-  WorkspaceMemberCredential({required this.userId, required this.workspaceId});
+  WorkspaceMemberCredential(
+      {required this.userId,
+      required this.workspaceId,
+      required this.userName,
+      required this.userRole});
 }
