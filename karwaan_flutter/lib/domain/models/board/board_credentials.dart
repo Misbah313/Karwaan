a4f5153d74d569cc4de958aec253adf45e7e9e57
Misbah Golami <misbahgholami63@gmail.com>
2025-08-11 18:39:38 +0330

    Added board credentails model.

---
+++ karwaan_flutter/lib/domain/models/board/board_credentials.dart
@@ -0,0 +1,10 @@
+class BoardCredentials {
+  final int id;
+  final String boardName;
+  final String boardDescription;
+
+  BoardCredentials(
+      {required this.id,
+      required this.boardName,
+      required this.boardDescription});
+}
