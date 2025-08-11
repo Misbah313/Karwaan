90c5564457f2b10e977f9aa786ae71a2e2036ab4
Misbah Golami <misbahgholami63@gmail.com>
2025-08-11 18:39:11 +0330

    Added board model.

---
+++ karwaan_flutter/lib/domain/models/board/board.dart
@@ -0,0 +1,15 @@
+class Board {
+  final int id;
+  final String boardName;
+  final String boardDescription;
+  final DateTime createAt;
+  // final String createdBy;
+
+  Board(
+      {required this.id,
+      required this.boardName,
+      required this.boardDescription,
+      required this.createAt,
+      // required this.createdBy
+      });
+}
