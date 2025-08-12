cff92993ead3898c8e5b292eaa3e08bcce2a42a0
Misbah Golami <misbahgholami63@gmail.com>
2025-08-12 23:17:18 +0330

    Added boardlist credentials model.

---
+++ karwaan_flutter/lib/domain/models/boardlist/create_board_list_credentails.dart
@@ -0,0 +1,10 @@
+class CreateBoardListCredentails {
+  final int boardListId;
+  final String boardListName;
+  final DateTime createdAt;
+
+  CreateBoardListCredentails(
+      {required this.boardListId,
+      required this.boardListName,
+      required this.createdAt});
+}
