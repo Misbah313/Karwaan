1d0e985583925fa45e09883b36661241930e2eda
Misbah Golami <misbahgholami63@gmail.com>
2025-08-14 19:15:21 +0330

    Add specific board card credentails.

---
+++ karwaan_flutter/lib/domain/models/boardcard/board_card_credentails.dart
@@ -0,0 +1,12 @@
+class BoardCardCredentails {
+  final int cardId;
+  final String newTitle;
+  final String newDec;
+  final bool isCompleted;
+
+  BoardCardCredentails(
+      {required this.cardId,
+      required this.newTitle,
+      required this.newDec,
+      required this.isCompleted});
+}
