96fa54e722cba4e31f580546671adbedd1734c07
Misbah Golami <misbahgholami63@gmail.com>
2025-08-18 20:47:41 +0330

    Added label specific model.

---
+++ karwaan_flutter/lib/domain/models/label/label.dart
@@ -0,0 +1,8 @@
+class Label {
+  final int? id;
+  final int boardId;
+  final String title;
+  final String color;
+
+  Label({required this.id,required this.boardId, required this.title, required this.color});
+}
