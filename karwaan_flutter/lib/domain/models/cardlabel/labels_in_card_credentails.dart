8290cff20f8e0a608bbc41da8aafaba845957cd2
Misbah Golami <misbahgholami63@gmail.com>
2025-08-18 20:46:32 +0330

    Added specifc labels in card credentails models.

---
+++ karwaan_flutter/lib/domain/models/cardlabel/labels_in_card_credentails.dart
@@ -0,0 +1,8 @@
+class LabelsInCardCredentails {
+  final int id;
+  final String title;
+  final String color;
+
+  LabelsInCardCredentails(
+      {required this.id, required this.title, required this.color});
+}
