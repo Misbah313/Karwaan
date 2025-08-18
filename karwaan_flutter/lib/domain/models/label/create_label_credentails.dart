4daa93f3903cb12078433b7ea1868e0c877f4f66
Misbah Golami <misbahgholami63@gmail.com>
2025-08-18 20:47:06 +0330

    Added specific cardlable create credentails model.

---
+++ karwaan_flutter/lib/domain/models/label/create_label_credentails.dart
@@ -0,0 +1,8 @@
+class CreateLabelCredentails {
+  final int boardId;
+  final String title;
+  final String color;
+
+  CreateLabelCredentails(
+      {required this.boardId, required this.title, required this.color});
+}
