082d91fb71c31f04e665b8d116c03104cbfebd40
Misbah Golami <misbahgholami63@gmail.com>
2025-08-22 15:22:31 +0330

    Added comment cubit for state managment.

---
+++ karwaan_flutter/lib/presentation/cubits/comment/comment_cubit.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:karwaan_flutter/domain/models/comment/comment_state.dart';
+import 'package:karwaan_flutter/domain/models/comment/create_comment_credentails.dart';
+import 'package:karwaan_flutter/domain/models/comment/update_comment_credentails.dart';
+import 'package:karwaan_flutter/domain/repository/comment/comment_repo.dart';
+
+class CommentCubit extends Cubit<CommentState> {
+  final CommentRepo commentRepo;
+
+  CommentCubit(this.commentRepo) : super(CommentInitial());
+
+  // create comment
+  Future<void> createComment(CreateCommentCredentails credentails) async {
+    emit(CommentLoading());
+    try {
+      final created = await commentRepo.createComment(credentails);
+      emit(CommentCreated(created));
+    } catch (e) {
+      emit(CommentError('Failed from cubit: ${e.toString()}'));
+    }
+  }
+
+  // get comments for card
+  Future<void> getCommentsForCard(int cardId) async {
+    emit(CommentLoading());
+    try {
+      final comments = await commentRepo.getCommentsForCard(cardId);
+      emit(CommentForCardListLoaded(comments));
+    } catch (e) {
+      emit(CommentError('Failed from cubit: ${e.toString()}'));
+    }
+  }
+
+  // update comment
+  Future<void> udpateComment(UpdateCommentCredentails credentails) async {
+    emit(CommentLoading());
+    try {
+      final updated = await commentRepo.updateComment(credentails);
+      emit(CommentUpdated(updated));
+    } catch (e) {
+      emit(CommentError('Failed from cubit: ${e.toString()}'));
+    }
+  }
+
+  // delete comment
+  Future<void> deleteComment(int commentId) async {
+    emit(CommentLoading());
+    try {
+      await commentRepo.deleteComment(commentId);
+      emit(CommentDeleted(commentId));
+    } catch (e) {
+      emit(CommentError('Failed from cubit: ${e.toString()}'));
+    }
+  }
+}
