eae0a52187cc540cd94a8390b8c134c2ccb27f91
Misbah Golami <misbahgholami63@gmail.com>
2025-08-29 00:27:49 +0330

    Create and apply new migrations.

---
+++ karwaan_server/migrations/20250825181711653/migration.sql
@@ -0,0 +1,25 @@
+BEGIN;
+
+--
+-- ACTION ALTER TABLE
+--
+ALTER TABLE "user" ADD COLUMN "profileImage" text;
+
+--
+-- MIGRATION VERSION FOR karwaan
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('karwaan', '20250825181711653', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20250825181711653', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20240516151843329', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240516151843329', "timestamp" = now();
+
+
+COMMIT;
