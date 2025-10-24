b8d5d78539800214a30291a45ad5575882cd9f4e
Misbah Golami <misbahgholami63@gmail.com>
2025-10-24 22:36:56 +0330

    Initialize .env

--- karwaan_flutter/lib/main.dart
+++ karwaan_flutter/lib/main.dart
@@ -1,6 +1,7 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:karwaan_flutter/core/services/auth_token_storage_helper.dart';
 import 'package:karwaan_flutter/core/services/serverpod_client_service.dart';
 import 'package:karwaan_flutter/core/theme/dark_mode.dart';
@@ -35,6 +36,7 @@ import 'package:karwaan_flutter/presentation/cubits/auth/auth_gate.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
+  await dotenv.load(fileName: '.env'); 
   WidgetsFlutterBinding.ensureInitialized();
 
   final serverpodClientService =
