47fc135f95b6276a0ad800759ebd7542fbb58a4d
Misbah Golami <misbahgholami63@gmail.com>
2025-10-24 20:31:11 +0330

    Added backgroundColor and isPrivate fields.

--- karwaan_flutter/lib/domain/models/workspace/create_workspace_credentials.dart
+++ karwaan_flutter/lib/domain/models/workspace/create_workspace_credentials.dart
@@ -2,7 +2,13 @@ class CreateWorkspaceCredentials {
   final String workspaceName;
   final String workspaceDescription;
   final DateTime createdAt;
+  final String backgroundColor;
+  final bool isPrivate;
 
   CreateWorkspaceCredentials(
-      {required this.workspaceName, required this.workspaceDescription, required this.createdAt});
+      {required this.workspaceName,
+      required this.workspaceDescription,
+      required this.createdAt,
+      this.backgroundColor = '#6B7280',
+      this.isPrivate = false});
 }
