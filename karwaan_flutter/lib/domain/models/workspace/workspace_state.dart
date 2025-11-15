09109de46a65395aea61caaf6940efbf744c9482
Misbah Golami <misbahgholami63@gmail.com>
2025-11-16 00:15:14 +0330

    Remove the ununnecessary states.

--- karwaan_flutter/lib/domain/models/workspace/workspace_state.dart
+++ karwaan_flutter/lib/domain/models/workspace/workspace_state.dart
@@ -6,37 +6,18 @@ class WorkspaceInitial extends WorkspaceState {}
 
 class WorkspaceLoading extends WorkspaceState {}
 
-class SingleWorkspaceLoaded extends WorkspaceState {
-  final Workspace workspace;
-  SingleWorkspaceLoaded(this.workspace);
-}
-
 class WorkspaceListLoaded extends WorkspaceState {
   final List<Workspace> workspaces;
 
   WorkspaceListLoaded(this.workspaces);
 }
 
-class SuccessAction extends WorkspaceState {
+class WorkspaceCreated extends WorkspaceState {
   final String workspaceName;
-  final String workspaceDec;
 
-  SuccessAction(this.workspaceName, this.workspaceDec);
+  WorkspaceCreated(this.workspaceName);
 }
-
-class DeletedSuccessfully extends WorkspaceState {
-  final int workspaceId;
-
-  DeletedSuccessfully(this.workspaceId);
-}
-
-class WorkspaceNotLoaded extends WorkspaceState {}
-
 class WorkspaceError extends WorkspaceState {
   final String error;
   WorkspaceError(this.error);
-}
-
-class WorkspaceUpdated extends WorkspaceState {
-  
 }
