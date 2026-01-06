abfd2c7f62faab28a7d6d512f3d4c45e69cdaf68
Misbah Golami <misbahgholami63@gmail.com>
2026-01-06 14:47:00 +0330

    Added optimized methods for update and delete boards.

--- karwaan_flutter/lib/presentation/cubits/board/board_cubit.dart
+++ karwaan_flutter/lib/presentation/cubits/board/board_cubit.dart
@@ -42,11 +42,56 @@ class BoardCubit extends Cubit<BoardState> {
     }
   }
 
+  Future<void> updateBoardOptimized(
+      BoardCredentials credentails, int worksapceId) async {
+    try {
+      await boardRepo.updateBoard(credentails);
+
+      // if board already loaded, update just the one that changed
+      if (state is BoardlistLoaded) {
+        final currentState = state as BoardlistLoaded;
+        final updatedBoard = currentState.boards.map((boards) {
+          if (boards.id == credentails.id) {
+            return boards.copyWith(
+              boardName: credentails.boardName,
+              boardDescription: credentails.boardDescription,
+            );
+          }
+
+          return boards;
+        }).toList();
+        emit(BoardlistLoaded(updatedBoard));
+      } else {
+        await getBoardsByWorkspace(worksapceId);
+      }
+    } catch (e) {
+      emit(BoardError(ExceptionMapper.toMessage(e)));
+    }
+  }
+
   Future<void> deleteBoard(int boardId) async {
     emit(BoardLoading());
     try {
       await boardRepo.deleteBoard(boardId);
       emit(DeletedSuccessfully(boardId));
+    } catch (e) {
+      emit(BoardError(ExceptionMapper.toMessage(e)));
+    }
+  }
+
+  Future<void> deleteBoardOptimized(int boardId, int worksapceId) async {
+    try {
+      await boardRepo.deleteBoard(boardId);
+
+      // if board already loaded, remove just the deleted one
+      if (state is BoardlistLoaded) {
+        final currentState = state as BoardlistLoaded;
+        final updatedBoards =
+            currentState.boards.where((board) => board.id != boardId).toList();
+        emit(BoardlistLoaded(updatedBoards));
+      } else {
+        await getBoardsByWorkspace(worksapceId);
+      }
     } catch (e) {
       emit(BoardError(ExceptionMapper.toMessage(e)));
     }
