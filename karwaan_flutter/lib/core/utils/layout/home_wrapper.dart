361f7abb04db4d1aeb34fd92f3421a0d3753edb1
Misbah Golami <misbahgholami63@gmail.com>
2026-01-06 14:39:35 +0330

    Provide the mainLayoutCubit for access.

--- karwaan_flutter/lib/core/utils/layout/home_wrapper.dart
+++ karwaan_flutter/lib/core/utils/layout/home_wrapper.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:karwaan_flutter/core/services/workspace/main_layout_cubit.dart';
+import 'package:karwaan_flutter/presentation/cubits/main_layout_cubit.dart';
 import 'package:karwaan_flutter/core/utils/layout/responsive_layout.dart';
 import 'package:karwaan_flutter/presentation/pages/desktop/workspace/main_app_page.dart';
 import 'package:karwaan_flutter/presentation/pages/mobile/workspace/home_page.dart';
